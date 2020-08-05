c849200defaa35ce205f8c1b934002ff0272d36f
danhlogan <logan0839@student.cptc.edu>
2020-08-05 11:39:56 -0700

    Added course class

---
+++ NestedObjects/Course.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Course
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public byte Credits { get; set; }
+
+        /// <summary>
+        /// Instructor that teaches the course
+        /// </summary>
+        public Instructor CourseInstructor { get; set; }
+
+
+        /// <summary>
+        /// All the students currently enrolled in the class
+        /// </summary>
+        public List<Student> Roster { get; set; }
+
+
+    }
+}
